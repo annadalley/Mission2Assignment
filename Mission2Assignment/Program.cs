da815a7370cb26f84e77edbbf8240e9421d476ff
annadalley <annadalley106@gmail.com>
2023-01-18 21:06:17 -0700

    Finished Program

--- Mission2Assignment/Program.cs
+++ Mission2Assignment/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-
+//Anna Dalley
+//Section 1
+//Group 4
 namespace Mission2Assignment
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create new object and variables
+            ThrowDice td = new ThrowDice();
+            int numThrows = 0;
+
+            //string to welcome to program and prompt user how many dice rolls they want
+            Console.WriteLine("Welcome to the dice throwing simulator!\n");
+            Console.Write("How many dice rolls would you like to simulate? ");
+            
+            //takes input from user, changes from string to an integer, and assigns it to the variable initialized above.
+            numThrows = int.Parse(Console.ReadLine());
+
+            //Begins printing results
+            Console.Write("\nDICE ROLLING SIMULATION RESULTS\nEach \"*\" represents 1% of the total number of rolls.\nTotal number of rolls = " + numThrows + ".\n\n");
+
+            //calls method in the ThrowDice class passing in numRolls and returns results
+            td.NewRoll(numThrows);
+
+            //Thanks user for playing
+            Console.WriteLine("\nThank you for using the dice throwing simulator! Goodbye.");
         }
     }
 }
---
+++ Mission2Assignment/ThrowDice.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission2Assignment
+{
+    public class ThrowDice
+    {
+        //creates an array to hold the number of times each potential total was rolled
+        int[] aThrow = new int[11];
+        
+        //creates a new method called newRoll that accepts the parameter of numRolls
+        public void NewRoll(int rolls)
+        {
+            //for loop to create 2 random numbers between 1 and less than 7 for each roll
+            //adds the 2 random numbers together to get the dice total.
+            for (int iCount = 0; iCount < rolls; iCount++)
+            {
+                Random rnd1 = new Random();
+                Random rnd2 = new Random();
+                int die1 = rnd1.Next(1, 7);
+                int die2 = rnd2.Next(1, 7);
+                int total = (die1 + die2);
+
+                //this takes the total minus 2 to get the position of the total in the array, and adds 1 to the current value in that position.
+                //example: total = 2.  array[0] = Array[0]+1.
+                aThrow[total-2] = aThrow[total-2] + 1;
+                
+            }
+            //for loop to start at 2, and create the display starting from 2: to 12:
+            for (int iCount2 = 2; iCount2 < 13; iCount2++)
+            {
+                Console.Write(iCount2 + ": ");
+                //calculates number of stars by taking the value in the array and dividing it by 1% times the number of rolls
+                double dStars = (aThrow[iCount2 - 2])/(.01 * rolls);
+                //converts number of stars from a decimal to an integer
+                int iStars = Convert.ToInt32(dStars);
+                //writes a star equal to the number of stars calculated above for each total.
+                for (int iCount3 = 0; iCount3 < iStars; iCount3++)
+                {
+                    Console.Write("*");
+                }
+                //adds a line after all the stars for each number are added.
+                Console.Write("\n");
+            }
+        }
+    }
+}
+
+
